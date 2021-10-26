8b8cfdfec07ca81b4718aa58e859967544638c8b
techsolutionsofficial <91885730+techsolutionsofficial@users.noreply.github.com>
2021-10-26 22:16:35 +0500

    New Commit

---
+++ Techtree/Areas/Admin/Data/Basic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Techtree.Areas.Admin.Data
+{
+    public class Basic
+    {
+    }
+}
---
+++ Techtree/Areas/Admin/Models/BasicContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Techtree.Areas.Admin.Models
+{
+    public class BasicContext
+    {
+    }
+}
